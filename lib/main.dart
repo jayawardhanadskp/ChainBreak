35169fd7e25a64b3c3fcb31da6b2d0283326218f
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-11-08 16:24:55 +0530

    main

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:chain_break/firebase_options.dart';
+import 'package:chain_break/providers/process_provider.dart';
+import 'package:chain_break/screens/authentication/login_screen.dart';
+import 'package:chain_break/screens/authentication/sign_up_screen.dart';
+import 'package:chain_break/screens/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ProgressProvider(),
+        )
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: SignUpScreen());
+  }
+}
