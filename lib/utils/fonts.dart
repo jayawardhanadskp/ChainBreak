5eb1b38aa63390cb5993d6cd45f739043c035944
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-11-13 20:07:03 +0530

    firebase options

--- lib/utils/fonts.dart
+++ lib/utils/fonts.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Fonts {
-  static TextStyle get headings =>
-      GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.w600);
+  static TextStyle large(Color color) =>
+      GoogleFonts.bebasNeue(fontSize: 32, fontWeight: FontWeight.w600);
+  static TextStyle headings(Color color) =>
+      GoogleFonts.bebasNeue(fontSize: 32, fontWeight: FontWeight.w600);
 
-  static TextStyle get normal => GoogleFonts.poppins(fontSize: 15);
-  static TextStyle get normalWhite => GoogleFonts.poppins(fontSize: 15, color: Colors.white);
+  static TextStyle  normal(Color color) => GoogleFonts.poppins(fontSize: 15);
+  static TextStyle  normalWhite(Color color) => GoogleFonts.poppins(fontSize: 15, color: Colors.white);
 
-  static TextStyle get normalSmall => GoogleFonts.poppins(fontSize: 14);
+  static TextStyle  normalSmall(Color color) => GoogleFonts.poppins(fontSize: 14);
 }
