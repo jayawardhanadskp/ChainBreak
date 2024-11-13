fa4fbffcab3b35aedd504d7fad7405e1eceacc50
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-11-13 20:06:34 +0530

    colors

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color get blue => const  Color(0xFF2192FF);
+  static Color get blue => const Color(0xFF2192FF);
+
+  static Color get blueee => const Color(0xFFBFECFFF);
+  static Color get purple => const Color(0xFFCDC1FF);
+  static Color get primary => const Color(0xFFFFF6E3);
+  static Color get pink => const Color(0xFFFFCCEA);
+  
+  static Color get darkblueee => const Color(0xFF2A0944);
+  static Color get darkpurple => const Color(0xFF3B185F);
+  static Color get darkprimary => const Color(0xFFFEC260);
+  static Color get darkpink => const Color(0xFFA12568);
+
+  // on board screen colors
 }
