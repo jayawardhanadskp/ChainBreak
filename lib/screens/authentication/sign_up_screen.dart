13ec48680da531a932f074ebde6e274d305b4b70
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-11-13 20:08:20 +0530

    signup screen

--- lib/screens/authentication/sign_up_screen.dart
+++ lib/screens/authentication/sign_up_screen.dart
@@ -1,9 +1,11 @@
-
+import 'package:chain_break/screens/authentication/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:chain_break/providers/auth_provider.dart'; // Import AuthProvider
 import 'package:chain_break/utils/fonts.dart';
 import 'package:chain_break/utils/image_strings.dart';
 import 'package:chain_break/widgets/app_button.dart';
 import 'package:chain_break/widgets/form_fields.dart';
-import 'package:flutter/material.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -13,21 +15,21 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  TextEditingController firstNameController = TextEditingController();
+  TextEditingController lastNameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  bool isObsecure = true;
+
+  void togglePasswordVisibility() => setState(() {
+        isObsecure = !isObsecure;
+      });
+
   @override
   Widget build(BuildContext context) {
-    TextEditingController firstNameController = TextEditingController();
-    TextEditingController lastNameController = TextEditingController();
-    TextEditingController emailController = TextEditingController();
-    TextEditingController passwordController = TextEditingController();
-
-    bool isObsecure = true;
-
-    void togglePasswordVisibility() => setState(() {
-          isObsecure = !isObsecure;
-        });
-
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(title: const Text('Sign Up')),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20.0),
         child: Column(
@@ -38,7 +40,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
             ),
             Text(
               'Join ChainBreak!',
-              style: Fonts.headings,
+              style: Theme.of(context).textTheme.displayLarge,
             ),
             const SizedBox(
               height: 30,
@@ -82,15 +84,63 @@ class _SignUpScreenState extends State<SignUpScreen> {
               isObsecure: isObsecure,
               icon: IconButton(
                 icon: isObsecure
-                  ? const Icon(Icons.visibility_off)
-                  : const Icon(Icons.visibility),
+                    ? const Icon(Icons.visibility_off)
+                    : const Icon(Icons.visibility),
                 onPressed: togglePasswordVisibility,
               ),
             ),
             const SizedBox(
               height: 30,
             ),
-            AppButton(onTap: () {}, bText: 'Create Account')
+            // Create Account Button
+            AppButton(
+              onTap: () async {
+                final String email = emailController.text.trim();
+                final String password = passwordController.text.trim();
+                final String firstName = firstNameController.text.trim();
+                final String lastName = lastNameController.text.trim();
+
+                if (email.isEmpty ||
+                    password.isEmpty ||
+                    firstName.isEmpty ||
+                    lastName.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Please fill in all fields')),
+                  );
+                  return;
+                }
+
+                // Call signUp from AuthProvider with first and last name
+                await Provider.of<AuthProvider>(context, listen: false)
+                    .signUp(email, password, firstName, lastName);
+
+                // Check if sign up was successful
+                if (Provider.of<AuthProvider>(context, listen: false)
+                    .isAuthenticated) {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()));
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Sign-up failed')),
+                  );
+                }
+              },
+              bText: 'Create Account',
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Text('Have an Account ?'),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => LoginScreen()));
+                },
+                child: Text("Sign In"))
+              ],
+            )
           ],
         ),
       ),
