ff0069490ada360ed2471fad9bca74972e9ed3b7
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-11-14 21:32:14 +0530

    wellness screen

---
+++ lib/screens/welness_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WelnessScreen extends StatelessWidget {
+  const WelnessScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
